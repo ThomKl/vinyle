f06941189b111b1fe9c895f430dc0b612c046f34
Thomas Klapper <thomasklapper@orange.fr>
2018-11-02 15:59:02 +0100

    new controller

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,15 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController {
+   
+    /**Permet de déclarer une route sans passer par le fichier routes.yml **/
     /**
     * @Route("/")
     */
     
     public function homepage() {
         return new Response('lol');
+    }
+    
+    /**
+    * @Route("/article")
+    */
+    public function showArticle() {
+        return new Response('voici un article');
     }
     
 }
