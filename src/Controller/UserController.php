9cdc2c2781ee384432f4ca3a9226d9441abb4949
Thomas Klapper <thomasklapper@orange.fr>
2018-11-02 12:26:54 +0100

    update

---
+++ src/Controller/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController {
+    
+    public function user() {
+        return new Response('lol');
+    }
+    
+}
+   
