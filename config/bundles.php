bd0e893773468912218ae5865e5b2bfc6a72a9a7
Thomas Klapper <thomasklapper@orange.fr>
2018-11-04 16:39:32 +0100

    profiler

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ArticleController {
    
@@ -13,7 +14,16 @@ class ArticleController {
     */
     
     public function homepage() {
-        return new Response('lol');
+        return new Response('homepage');
     }
+   
+    
+    /**
+    * @Route("/article/{nomProduit}")
+    */
+    public function pageProduct($nomProduit) {
+        return new Response("<html><body>page produit : " . $nomProduit . "</body></html>");
+    }
+
 }
     
